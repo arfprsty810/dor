394c86026d912ff3fa1fbc0c39e678d0f76a1621
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-11-26 07:30:58 +0700

    Add files via upload

--- dor.sh
+++ dor.sh
@@ -16,8 +16,10 @@ echo ""
 echo "[★]====[ ★=== Select Menu ===★ ]====[★]"
 echo ""
 echo "[★] [ 1 ] [★] Axis"
-echo "[★] [ 2 ] [★] Three"
-echo "[★] [ 99 ] [★] Keluar"
+echo "[★] [ 2 ] [★] XL"
+echo "[★] [ 3 ] [★] Three - 1"
+echo "[★] [ 4 ] [★] Three FULL"
+echo "[★] [ 0 ] [★] Keluar"
 echo ""
 echo "[★]====[ ★=== Select Menu ===★ ]====[★]"
 echo "──────────────────────────────────────────────────"$r
@@ -28,14 +30,24 @@ sleep 2
 if [ $pilih -eq 1 ]
 then
     clear
-    php ~/dor/axis/axis.php
+    php ~/dor/axiata/axis.php
     
 elif [ $pilih -eq 2 ]
 then
     clear
+    php ~/dor/axiata/xl.php
+    
+elif [ $pilih -eq 3 ]
+then
+    clear
     php ~/dor/bima/tri.php
 
-elif [ $pilih -eq 99 ]
+elif [ $pilih -eq 4 ]
+then
+    clear
+    php ~/dor/bima/dor3full.php
+
+elif [ $pilih -eq 0 ]
 then
     echo "[!] Keluar";
     echo ""
--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/sh
 
 # Install Requirements
+cd ~
 pkg update && pkg upgrade -y
 pkg install git wget bash curl python php -y
 
-cd
 rm -rf dor
 rm -rf *.sh
 git clone https://github.com/arfprsty810/dor.git
 cd dor
 
 chmod +x dor.sh
-chmod +x axis/axis.php
+chmod +x axiata/axis.php
+chmod +x axiata/xl.php
 chmod +x bima/tri.php
+chmod +x bima/dor3full.php
 
 cd
